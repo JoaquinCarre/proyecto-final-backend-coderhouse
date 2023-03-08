8d89bf517c169882bb239503102cbe989417c9da
Joaquín Carré <joa_carre21@hotmail.com>
2023-03-08 19:02:38 -0300

    Cambiando obtención MongoDB URI

--- config/mongoDB.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config.js';
-
-export default {
-    mongoDB: {
-        URI: `mongodb+srv://joacarre21:${process.env.MONGO_PASS}@cluster0.del2hb6.mongodb.net/eccomerce?retryWrites=true&w=majority`
-    },
-}
--- models/dao/containers/mongoDBContainer.js
+++ models/dao/containers/mongoDBContainer.js
@@ -1,73 +1,72 @@
 import { logger } from "../../../logs/logger.js";
 import mongoose from 'mongoose';
-import config from '../../../config/mongoDB.js';
 
 const advancedOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
 
-mongoose.connect(config.mongoDB.URI, advancedOptions);
+mongoose.connect(process.env.MONGO_URI, advancedOptions);
 
 class MongoDBContainer {
-    constructor(collection) {
-      this.collection = collection;
-    }
-  
-    async create(data) {
-      try {
-        return await this.collection.create(data);
-      } catch (err) {
-        logger.error('No es posible crear la base de datos ', err);
-        const customError = new Error(`No es posible crear la base de datos: ${err}`);
-        customError.id = 4;
-        next(customError);
-      }
-    }
-  
-    async getAll() {
-      try {
-        return await this.collection.find({}).lean();
-      } catch (err) {
-        logger.error('No es posible obtener la información de la base de datos ', err);
-        const customError = new Error(`No es posible obtener la información de la base de datos: ${err}`);
-        customError.id = 4;
-        next(customError);
-      }
-    }
-  
-    async getOneById(id) {
-      try {
-        return await this.collection.findById(id);
-      } catch (err) {
-        logger.error('No es posible obtener el elemento buscado de la base de datos ', err);
-        const customError = new Error(`No es posible obtener el elemento buscado de la base de datos: ${err}`);
-        customError.id = 4;
-        next(customError);
-      }
-    }
-  
-    async updateById(id, data) {
-      try {
-        return await this.collection.updateOne({ _id: id }, { $set: data });
-      } catch (err) {
-        logger.error('No es posible actualizar el elemento en la base de datos ', err);
-        const customError = new Error(`No es posible actualizar el elemento en la base de datos: ${err}`);
-        customError.id = 4;
-        next(customError);
-      }
-    }
-  
-    async deleteById(id) {
-      try {
-        return await this.collection.deleteOne({ _id: id });
-      } catch (err) {
-        logger.error('No es posible borrar el elemento de la base de datos ', err);
-        const customError = new Error(`No es posible borrar el elemento de la base de datos: ${err}`);
-        customError.id = 4;
-        next(customError);
-      }
+  constructor(collection) {
+    this.collection = collection;
+  }
+
+  async create(data) {
+    try {
+      return await this.collection.create(data);
+    } catch (err) {
+      logger.error('No es posible crear la base de datos ', err);
+      const customError = new Error(`No es posible crear la base de datos: ${err}`);
+      customError.id = 4;
+      next(customError);
     }
   }
-  
-  export default MongoDBContainer;
+
+  async getAll() {
+    try {
+      return await this.collection.find({}).lean();
+    } catch (err) {
+      logger.error('No es posible obtener la información de la base de datos ', err);
+      const customError = new Error(`No es posible obtener la información de la base de datos: ${err}`);
+      customError.id = 4;
+      next(customError);
+    }
+  }
+
+  async getOneById(id) {
+    try {
+      return await this.collection.findById(id);
+    } catch (err) {
+      logger.error('No es posible obtener el elemento buscado de la base de datos ', err);
+      const customError = new Error(`No es posible obtener el elemento buscado de la base de datos: ${err}`);
+      customError.id = 4;
+      next(customError);
+    }
+  }
+
+  async updateById(id, data) {
+    try {
+      return await this.collection.updateOne({ _id: id }, { $set: data });
+    } catch (err) {
+      logger.error('No es posible actualizar el elemento en la base de datos ', err);
+      const customError = new Error(`No es posible actualizar el elemento en la base de datos: ${err}`);
+      customError.id = 4;
+      next(customError);
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      return await this.collection.deleteOne({ _id: id });
+    } catch (err) {
+      logger.error('No es posible borrar el elemento de la base de datos ', err);
+      const customError = new Error(`No es posible borrar el elemento de la base de datos: ${err}`);
+      customError.id = 4;
+      next(customError);
+    }
+  }
+}
+
+export default MongoDBContainer;
